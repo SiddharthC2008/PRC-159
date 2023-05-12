b57a1755691f0ddc32052cb099548bb78e92dc52
SiddharthC2008 <93938307+SiddharthC2008@users.noreply.github.com>
2023-05-12 15:58:48 -0400

    Add files via upload

---
+++ Highlight.js
@@ -0,0 +1,14 @@
+
+update: function () {
+const fadeBackgroundE1 = document.querySelector("#fadeBackground");
+C = fadeBackgroundEl.children;
+if (c.length > 0) {
+var i;
+for (i=0; i <= c.length; i++) 
+{ 
+    fadeBackgroundEl.removeChild(c[i]);
+}
+} else {
+this.handleMouseClickEvents();
+}
+},
